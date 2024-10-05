ca7745ed2ec36bf401d11a6ebca0db4512f1ff06
ivajon <ivar.jonsson18@gmail.com>
2024-10-05 16:27:09 +0200

    Continuation of the board support package

---
+++ bsp/src/constants.rs
@@ -0,0 +1,52 @@
+//! Defines a few board specific constants.
+
+use nrf52840_hal::saadc;
+/// Resistance to ground for the current sense pins.
+pub const R_PHASE_SENSE_TO_GROUND: f32 = 10_000.;
+
+/// Resistance to vin for the current sense pins.
+pub const R_PHASE_SENSE_TO_VIN: f32 = 4_700.;
+
+/// The shunt resistor that the current sense measures voltage across.
+pub const R_SHUNT: f32 = 20.;
+
+/// The gain that the sensor applies to the differential measurement.
+pub const SENSE_GAIN: f32 = 50.;
+
+/// The system voltage.
+pub const V_REF: f32 = 3.3;
+
+/// The gain in the nrf chip.
+pub const ADC_BITS: usize = 14;
+
+/// The gain of the adc on the nrf chip.
+pub const ADC_GAIN: f32 = 1.0;
+
+/// The voltage divider used for current sensing.
+/// $I_shunt = (2.5-V_sense *  ( R_5+R_4 ) / R_5) / ( R_shunt  * NågonGain ) $
+pub const fn raw_adc_to_current_factor() -> f32 {
+    let v_sense = (V_REF / ((1 << ADC_BITS) as f32)) / ADC_GAIN;
+    (2.5 - v_sense * (R_PHASE_SENSE_TO_GROUND + R_PHASE_SENSE_TO_VIN) / R_PHASE_SENSE_TO_VIN)
+        / (R_SHUNT * SENSE_GAIN)
+}
+
+/// Converts the adc gain to the type representation at compile time.
+pub const fn adc_gain() -> saadc::Gain {
+    if ADC_GAIN == 1. / 6. {
+        saadc::Gain::GAIN1_6
+    } else if ADC_GAIN == 1. / 5. {
+        saadc::Gain::GAIN1_5
+    } else if ADC_GAIN == 1. / 4. {
+        saadc::Gain::GAIN1_4
+    } else if ADC_GAIN == 1. / 3. {
+        saadc::Gain::GAIN1_3
+    } else if ADC_GAIN == 1. / 2. {
+        saadc::Gain::GAIN1_2
+    } else if ADC_GAIN == 1. {
+        saadc::Gain::GAIN1
+    } else if ADC_GAIN == 2. {
+        saadc::Gain::GAIN2
+    } else {
+        panic!("Invalid ADC_GAIN")
+    }
+}
--- bsp/src/lib.rs
+++ bsp/src/lib.rs
@@ -1,5 +1,8 @@
 //! Defines a simple pin map for this specific board version.
 #![deny(warnings, missing_docs)]
+#![no_std]
+
+use constants::{adc_gain, raw_adc_to_current_factor};
 use nrf52840_hal::{
     gpio::{
         self,
@@ -10,6 +13,7 @@ use nrf52840_hal::{
     saadc::{SaadcConfig, Time},
     Saadc,
 };
+pub mod constants;
 pub mod events;
 
 /// The pins required to drive a single phase of the motor.
@@ -67,9 +71,9 @@ impl PinConfig<false, false, false, false> {
         let gpiote = nrf52840_hal::gpiote::Gpiote::new(gpiote);
         let conf = (ppi, gpiote);
         let (_ppi, gpiote) = conf
-            .enable_event_c0::<_, false>(&p1h)
-            .enable_event_c1::<_, false>(&p2h)
-            .enable_event_c2::<_, false>(&p3h);
+            .enable_event_c0::<_, true, true>(&p1h)
+            .enable_event_c1::<_, true, true>(&p2h)
+            .enable_event_c2::<_, true, true>(&p3h);
 
         let p1 = PhasePins {
             high_side: p0
@@ -138,21 +142,33 @@ trait EnableEvent {
     /// Enables events on channel c0.
     ///
     /// If low to high is set this triggers on rising edge other wise it triggers on falling edge.
-    fn enable_event_c0<T: nrf52840_hal::gpiote::GpioteInputPin, const LOW_TO_HIGH: bool>(
+    fn enable_event_c0<
+        T: nrf52840_hal::gpiote::GpioteInputPin,
+        const LOW_TO_HIGH: bool,
+        const HIGH_TO_LOW: bool,
+    >(
         self,
         pin: &T,
     ) -> Self;
     /// Enables events on channel c1.
     ///
     /// If low to high is set this triggers on rising edge other wise it triggers on falling edge.
-    fn enable_event_c1<T: nrf52840_hal::gpiote::GpioteInputPin, const LOW_TO_HIGH: bool>(
+    fn enable_event_c1<
+        T: nrf52840_hal::gpiote::GpioteInputPin,
+        const LOW_TO_HIGH: bool,
+        const HIGH_TO_LOW: bool,
+    >(
         self,
         pin: &T,
     ) -> Self;
     /// Enables events on channel c2.
     ///
     /// If low to high is set this triggers on rising edge other wise it triggers on falling edge.
-    fn enable_event_c2<T: nrf52840_hal::gpiote::GpioteInputPin, const LOW_TO_HIGH: bool>(
+    fn enable_event_c2<
+        T: nrf52840_hal::gpiote::GpioteInputPin,
+        const LOW_TO_HIGH: bool,
+        const HIGH_TO_LOW: bool,
+    >(
         self,
         pin: &T,
     ) -> Self;
@@ -160,7 +176,11 @@ trait EnableEvent {
     /// Enables events on channel c3.
     ///
     /// If low to high is set this triggers on rising edge other wise it triggers on falling edge.
-    fn enable_event_c3<T: nrf52840_hal::gpiote::GpioteInputPin, const LOW_TO_HIGH: bool>(
+    fn enable_event_c3<
+        T: nrf52840_hal::gpiote::GpioteInputPin,
+        const LOW_TO_HIGH: bool,
+        const HIGH_TO_LOW: bool,
+    >(
         self,
         pin: &T,
     ) -> Self;
@@ -168,7 +188,11 @@ trait EnableEvent {
     /// Enables events on channel c4.
     ///
     /// If low to high is set this triggers on rising edge other wise it triggers on falling edge.
-    fn enable_event_c4<T: nrf52840_hal::gpiote::GpioteInputPin, const LOW_TO_HIGH: bool>(
+    fn enable_event_c4<
+        T: nrf52840_hal::gpiote::GpioteInputPin,
+        const LOW_TO_HIGH: bool,
+        const HIGH_TO_LOW: bool,
+    >(
         self,
         pin: &T,
     ) -> Self;
@@ -239,6 +263,7 @@ impl<const P1: bool, const P2: bool, const P3: bool> PinConfig<P1, P2, P3, false
     ) -> (PinConfig<P1, P2, P3, false>, CurrentManager) {
         let mut cfg = SaadcConfig::default();
         cfg.time = Time::_3US;
+        cfg.gain = adc_gain();
         // TODO: Change this to a DMA version as to not block for 3us.
         let adc = Saadc::new(adc, cfg);
 
@@ -276,21 +301,32 @@ impl<const P1: bool, const P2: bool, const P3: bool, const ADC: bool> PinConfig<
 
 impl CurrentManager {
     /// Returns the current measurements in phase order.
-    pub fn sample(&mut self) -> Result<[i16; 3], ()> {
+    pub fn sample(&mut self) -> Result<[f32; 3], ()> {
         Ok([
-            self.adc.read_channel(&mut self.p1cs)?,
-            self.adc.read_channel(&mut self.p2cs)?,
-            self.adc.read_channel(&mut self.p3cs)?,
+            f32::from(self.adc.read_channel(&mut self.p1cs)?) * raw_adc_to_current_factor(),
+            f32::from(self.adc.read_channel(&mut self.p2cs)?) * raw_adc_to_current_factor(),
+            f32::from(self.adc.read_channel(&mut self.p3cs)?) * raw_adc_to_current_factor(),
         ])
     }
 }
 
 impl EnableEvent for (nrf52840_hal::ppi::Parts, nrf52840_hal::gpiote::Gpiote) {
-    fn enable_event_c0<T: nrf52840_hal::gpiote::GpioteInputPin, const LOW_TO_HIGH: bool>(
+    fn enable_event_c0<
+        T: nrf52840_hal::gpiote::GpioteInputPin,
+        const LOW_TO_HIGH: bool,
+        const HIGH_TO_LOW: bool,
+    >(
         mut self,
         pin: &T,
     ) -> Self {
-        if LOW_TO_HIGH {
+        if LOW_TO_HIGH && HIGH_TO_LOW {
+            self.1
+                .channel0()
+                .input_pin(pin)
+                .lo_to_hi()
+                .hi_to_lo()
+                .enable_interrupt();
+        } else if LOW_TO_HIGH {
             self.1
                 .channel0()
                 .input_pin(pin)
@@ -314,15 +350,27 @@ impl EnableEvent for (nrf52840_hal::ppi::Parts, nrf52840_hal::gpiote::Gpiote) {
         ppi0.enable();
         self
     }
-    fn enable_event_c1<T: nrf52840_hal::gpiote::GpioteInputPin, const LOW_TO_HIGH: bool>(
+    fn enable_event_c1<
+        T: nrf52840_hal::gpiote::GpioteInputPin,
+        const LOW_TO_HIGH: bool,
+        const HIGH_TO_LOW: bool,
+    >(
         mut self,
         pin: &T,
     ) -> Self {
-        if LOW_TO_HIGH {
+        if LOW_TO_HIGH && HIGH_TO_LOW {
             self.1
                 .channel1()
                 .input_pin(pin)
                 .lo_to_hi()
+                .hi_to_lo()
+                .enable_interrupt();
+        } else if LOW_TO_HIGH {
+            self.1
+                .channel1()
+                .input_pin(pin)
+                .lo_to_hi()
+                .hi_to_lo()
                 .enable_interrupt();
         } else {
             self.1
@@ -342,11 +390,22 @@ impl EnableEvent for (nrf52840_hal::ppi::Parts, nrf52840_hal::gpiote::Gpiote) {
         ppi.enable();
         self
     }
-    fn enable_event_c2<T: nrf52840_hal::gpiote::GpioteInputPin, const LOW_TO_HIGH: bool>(
+    fn enable_event_c2<
+        T: nrf52840_hal::gpiote::GpioteInputPin,
+        const LOW_TO_HIGH: bool,
+        const HIGH_TO_LOW: bool,
+    >(
         mut self,
         pin: &T,
     ) -> Self {
-        if LOW_TO_HIGH {
+        if LOW_TO_HIGH && HIGH_TO_LOW {
+            self.1
+                .channel2()
+                .input_pin(pin)
+                .lo_to_hi()
+                .hi_to_lo()
+                .enable_interrupt();
+        } else if LOW_TO_HIGH {
             self.1
                 .channel2()
                 .input_pin(pin)
@@ -370,11 +429,22 @@ impl EnableEvent for (nrf52840_hal::ppi::Parts, nrf52840_hal::gpiote::Gpiote) {
         ppi.enable();
         self
     }
-    fn enable_event_c3<T: nrf52840_hal::gpiote::GpioteInputPin, const LOW_TO_HIGH: bool>(
+    fn enable_event_c3<
+        T: nrf52840_hal::gpiote::GpioteInputPin,
+        const LOW_TO_HIGH: bool,
+        const HIGH_TO_LOW: bool,
+    >(
         mut self,
         pin: &T,
     ) -> Self {
-        if LOW_TO_HIGH {
+        if LOW_TO_HIGH && HIGH_TO_LOW {
+            self.1
+                .channel3()
+                .input_pin(pin)
+                .lo_to_hi()
+                .hi_to_lo()
+                .enable_interrupt();
+        } else if LOW_TO_HIGH {
             self.1
                 .channel3()
                 .input_pin(pin)
@@ -397,11 +467,22 @@ impl EnableEvent for (nrf52840_hal::ppi::Parts, nrf52840_hal::gpiote::Gpiote) {
         ppi.enable();
         self
     }
-    fn enable_event_c4<T: nrf52840_hal::gpiote::GpioteInputPin, const LOW_TO_HIGH: bool>(
+    fn enable_event_c4<
+        T: nrf52840_hal::gpiote::GpioteInputPin,
+        const LOW_TO_HIGH: bool,
+        const HIGH_TO_LOW: bool,
+    >(
         mut self,
         pin: &T,
     ) -> Self {
-        if LOW_TO_HIGH {
+        if LOW_TO_HIGH && HIGH_TO_LOW {
+            self.1
+                .channel4()
+                .input_pin(pin)
+                .lo_to_hi()
+                .hi_to_lo()
+                .enable_interrupt();
+        } else if LOW_TO_HIGH {
             self.1
                 .channel4()
                 .input_pin(pin)
